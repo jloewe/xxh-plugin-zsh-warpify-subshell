31c1c8e6d6ba03e6df6d5470ef71fbc0e58971ae
Jan Loewe <kontakt@jloewe.net>
2023-09-30 13:15:30 +0200

    Update pluginrc.zsh

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -2,3 +2,4 @@
 # Put here your functions, environment variables, aliases and whatever you need.
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "zsh"}}\x9c'
